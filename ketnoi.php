e888c2e7d7172cc99d4f5b6ab45a60fb0f7458db
nhanhl0902 <106294299+nhanhl0902@users.noreply.github.com>
2023-11-03 19:09:33 +0700

    Update ketnoi.php

--- ketnoi.php
+++ ketnoi.php
@@ -1,19 +1,20 @@
 
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname="test";
-// Create connection
-$conn=new mysqli($servername,$username,$password,$dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-$sql="INSERT INTO facebook(username,pass) VALUES('".$_POST['username']."', '".$_POST['pass']."')";
-if ($conn->query($sql) === TRUE) {
-    echo "LOGGED";
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
-$conn->close();
+// $servername = "localhost";
+// $username = "root";
+// $password = "root";
+// $dbname="test";
+// // Create connection
+// $conn=new mysqli($servername,$username,$password,$dbname);
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+//   }
+// $sql="INSERT INTO facebook(username,pass) VALUES('".$_POST['username']."', '".$_POST['pass']."')";
+// if ($conn->query($sql) === TRUE) {
+//     echo "LOGGED";
+//   } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+//   }
+// $conn->close();
+echo "logged";
 ?>
